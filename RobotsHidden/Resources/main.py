c4b00dc6cd56ebe259b4b0f536c1bfa2414fa968
Santiago Magniny <smagniny@c2r12s3.42madrid.com>
2025-10-25 13:50:18 +0200

    Dockerfiles for external software

--- RobotsHidden/Resources/main.py
+++ RobotsHidden/Resources/main.py
@@ -3,17 +3,21 @@ import json
 from bs4 import BeautifulSoup
 
 def recursive_autolist(url: str):
+    
     html = requests.get(url).text
     soup = BeautifulSoup(html, "html.parser")
 
-    links = [a['href'] for a in soup.find_all('a', href=True)]
+    links = [a['href'] for a in soup.find_all('a', href=True) ]
+        
+
     links = [link for link in links if link not in ("../", "./")]
 
     if links == ["README"]:
         readme_content = requests.get(f"{url}/README").text
         print(readme_content)
+        if "d5eec3ec36cf80dce44a896f961c1831a05526ec215693c8f2c39543497d4466" in readme_content:
+            exit()
         return None
-
     for link in links:
         recursive_autolist(f"{url}/{link}")
 
